9e2cfab95f8df2876e5a282545a5422d27881355
Gabin Darras <gabin.darras@ineat-conseil.fr>
2019-09-04 16:37:19 +0200

    feat(movie): add service & table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+// Services
+import { MoviesService } from './services/movies.service';
+
+// Models
+import { Movie } from './models/movie.models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-list';
+export class AppComponent implements OnInit {
+  loading: boolean;
+  movies$: Observable<Movie[]>;
+  constructor(private moviesService: MoviesService) { }
+
+  ngOnInit() {
+    this.movies$ = this.moviesService.getMovieList();
+  }
 }
